91991c1fd91fcd3d4ce096ed86613f1668ac9fcd
Simona-St <33196147+Simona-St@users.noreply.github.com>
2018-03-20 14:58:08 +0200

    Add files via upload

---
+++ replace lowercase characters with uppercase/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace replace_lowercase_characters_with_uppercase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Enter the string:");
+
+            var str = Console.ReadLine().ToCharArray();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                char letter = str[i];
+
+                if (char.IsLower(letter))
+                {
+                    letter = char.ToUpper(letter);
+                }
+
+                else if (char.IsUpper(letter))
+                {
+                    letter = char.ToLower(letter);
+                }
+
+                str[i] = letter;
+            }
+
+            Console.WriteLine("After conversion, the string is: ");
+            Console.WriteLine(String.Join("", str));
+
+        }
+    }
+}
---
+++ sort string using bubble sort/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace sort_string_using_bubble_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            string[] arr = new string[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = Console.ReadLine();
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                int caseSensitiveResult = string.Compare(arr[i - 1], arr[i], false);
+
+                if (caseSensitiveResult > 0) // value -1 indicates that arr[i-1] is “less than” arr[i]  
+                {
+                    string temp = arr[i - 1];
+                    arr[i - 1] = arr[i];
+                    arr[i] = temp;
+                }
+
+            }
+                Console.WriteLine(String.Join(" ", arr));
+        }
+    }
+}
